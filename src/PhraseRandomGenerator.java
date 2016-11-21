11a7811e0d8016aeacc3f285e9c361068a6e60ee
Nickpilgrim <nick.k@pilgrimconsulting.com>
2016-11-21 17:09:12 +0200

    Покращив генератор випадкових виразів

--- src/PhraseRandomGenerator.java
+++ src/PhraseRandomGenerator.java
@@ -1,20 +1,20 @@
 
 public class PhraseRandomGenerator {
     public static void main(String[] args) {
-        String[] wordListOne = {"цілодобовий", "круглий", "казковий", "цинічний", "адаптивний", "синхронізований", "інтерктивний", "завантижений"};
-        String[] wordLostTwo = {"упоноважений", "важкий", "чистий продукт", "центральний", "кастомізативний", "використний з вигодою", " банановий", "фічя", "дизайн", "перформенс"};
-        String[] wordLostTree = {"процес", "креатив", "підтримка", "біль", "знак", "застосування", "гнучкий процес", "залогуватися", "абстрактний", "клієнт"};
+        String[] wordListOne = {"Начальник", "Девелопер", "Тестер", "Тімлід", "Сішарпіст", "Айчар", "Прибиральник", "Бекенд розробник", "Фронтенд розробник"};
+        String[] wordListTwo = {"серйозний", "добрий", "класний", "досконалий", "упоротий", "мімімішний", "шалений", "рожевий", "роботящий", "офігіваючий", "красунчик"};
+        String[] woedListTree = {"перець", "байстрюк", "мудак", "розумник", "халтурщик", "лис", "слон", "супер герой", "довбатель", "солодкоїжка", "дрищь", "поц"};
 
         int oneLenght = wordListOne.length;
-        int twoLenght = wordLostTwo.length;
-        int treeLenght = wordLostTree.length;
+        int twoLenght = wordListTwo.length;
+        int treeLenght = woedListTree.length;
 
-        int rand1 = (int) (Math.random() * oneLenght);
-        int rand2 = (int) (Math.random() * twoLenght);
-        int rand3 = (int) (Math.random() * treeLenght);
+        int randomOne = (int) (Math.random() * oneLenght);
+        int randomTwo = (int) (Math.random() * twoLenght);
+        int randomTree = (int) (Math.random() * treeLenght);
 
-        String praze = wordListOne[rand1] + " " + wordLostTwo[rand2] + " " + wordLostTree[rand3];
-        System.out.println("Усе, що нам потрібно знати - це " + praze);
+        String prase = wordListOne[randomOne] + " " + wordListTwo[randomTwo] + " " + woedListTree[randomTree];
 
+        System.out.println("Усе, що нам потрібно знати - це що: " + prase);
     }
 }
